c17d0368f67e320f3e911aa8f79dcdd6099341f9
ybin <zyb0807@gmail.com>
2017-03-17 13:28:13 +0800

    initialize nginx framework

---
+++ src/core/nginx.c
@@ -0,0 +1,20 @@
+
+/*
+ * Copyright (C) Igor Sysoev
+ * Copyrignt (C) Nginx, Inc.
+ */
+
+
+#include <ngx_config.h>
+#include <ngx_core.h>
+#include <nginx.h>
+
+
+ngx_module_t  ngx_core_module = {
+};
+
+int 
+main(int argc, char *const *argv)
+{
+    return 0;
+}
---
+++ src/core/ngx_module.h
@@ -0,0 +1,15 @@
+
+/*
+ * Copyright (C) Igor Sysoev
+ * Copyright (C) Maxim Dounin
+ * Copyright (C) Nginx, Inc.
+ */
+
+
+#ifndef _NGX_MODULE_H_INCLUDED_
+#define _NGX_MODULE_H_INCLUDED_
+
+
+struct ngx_module_s {
+};
+#endif
